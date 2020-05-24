300dc89bb4f01741510aa0eadfedf546f2bdaf94
BrenoMaia98 <brenomaia98@gmail.com>
2020-05-24 18:55:50 -0300

    dashborad page with css and animations completed

--- front-end-web/src/routes/Dashboard/index.tsx
+++ front-end-web/src/routes/Dashboard/index.tsx
@@ -1,9 +1,100 @@
 import React from 'react';
 
-import {Title} from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/24214851?s=460&u=01ff12e09164ddf8d83fd6681c6a0fd6b4bf126f&v=4"
+            alt=" Breno Maia"
+          />
+          <div>
+            <strong>Glucon</strong>
+            <p>
+              Desenvolvimento do Trabalho de Conclusão de Curso II , segundo
+              semestre 2019
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/24214851?s=460&u=01ff12e09164ddf8d83fd6681c6a0fd6b4bf126f&v=4"
+            alt=" Breno Maia"
+          />
+          <div>
+            <strong>Glucon</strong>
+            <p>
+              Desenvolvimento do Trabalho de Conclusão de Curso II , segundo
+              semestre 2019
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/24214851?s=460&u=01ff12e09164ddf8d83fd6681c6a0fd6b4bf126f&v=4"
+            alt=" Breno Maia"
+          />
+          <div>
+            <strong>Glucon</strong>
+            <p>
+              Desenvolvimento do Trabalho de Conclusão de Curso II , segundo
+              semestre 2019
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/24214851?s=460&u=01ff12e09164ddf8d83fd6681c6a0fd6b4bf126f&v=4"
+            alt=" Breno Maia"
+          />
+          <div>
+            <strong>Glucon</strong>
+            <p>
+              Desenvolvimento do Trabalho de Conclusão de Curso II , segundo
+              semestre 2019
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/24214851?s=460&u=01ff12e09164ddf8d83fd6681c6a0fd6b4bf126f&v=4"
+            alt=" Breno Maia"
+          />
+          <div>
+            <strong>Glucon</strong>
+            <p>
+              Desenvolvimento do Trabalho de Conclusão de Curso II , segundo
+              semestre 2019
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
